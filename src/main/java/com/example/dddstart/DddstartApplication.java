07fe264fb9fc14201b5f7026dfc6de0e4ef94ff1
songii00 <wks4j1004@gmail.com>
2020-07-06 22:23:30 +0900

    init

---
+++ src/main/java/com/example/dddstart/DddstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.dddstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dddstart/DddstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
